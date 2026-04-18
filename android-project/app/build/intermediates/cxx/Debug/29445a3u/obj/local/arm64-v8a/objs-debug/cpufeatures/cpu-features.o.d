3ae49030c2428b87dfa212f7ad696991f4e4edfe
Diegulon <diegortizte@gmail.com>
2026-04-18 13:15:19 -0300

    untrack android build artifacts

--- android-project/app/build/intermediates/cxx/Debug/29445a3u/obj/local/arm64-v8a/objs-debug/cpufeatures/cpu-features.o.d
+++
@@ -1,5 +0,0 @@
-/home/terpodia/Documents/gb-emulator/android-project/app/build/intermediates/cxx/Debug/29445a3u/obj/local/arm64-v8a/objs-debug/cpufeatures/cpu-features.o: \
-  /home/terpodia/Android/Sdk/ndk/25.1.8937393/sources/android/cpufeatures/cpu-features.c \
-  /home/terpodia/Android/Sdk/ndk/25.1.8937393/sources/android/cpufeatures/cpu-features.h
-
-/home/terpodia/Android/Sdk/ndk/25.1.8937393/sources/android/cpufeatures/cpu-features.h:
